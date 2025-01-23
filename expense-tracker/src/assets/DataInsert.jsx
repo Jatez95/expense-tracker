18e66ed758cb84b5f0aa6e62b2e4152b83addc5c
Jatez95 <mateossanchezjavier@gmail.com>
2025-01-23 18:52:39 +0100

    Added a TODO

--- expense-tracker/src/assets/DataInsert.jsx
+++ expense-tracker/src/assets/DataInsert.jsx
@@ -1,6 +1,7 @@
 import Insert from "./Insert";
 
 export default function DataInsert() {
+    {/* TODO Add the thing that are left */}
     return (
         <div className='flex flex-row w-2/4 h-full mt-8 shadow-md rounded bg-stone-800 justify-center justify-self-center space-x-32'>
             <Insert label_title="Cantidad mes" input_placeholder="Cantidad mes"/>
